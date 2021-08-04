32e32e02c147bf909077fb0ec0142b4ddd71c86b
Hans <zthanson.20@ichat.sp.edu.sg>
2021-08-04 10:46:37 +0800

    Package Lock

--- server.js
+++ server.js
@@ -84,8 +84,7 @@ MongoClient.connect(url, (err, db) => {
     }
 });
 
-const PORT = process.env.PORT ||
-5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started and listening on port ${PORT}.`);
